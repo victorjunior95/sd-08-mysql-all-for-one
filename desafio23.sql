b10e9f4c3bd9bd89629afc8a4ada25b28303f74d
hugobrr <hugobrr@gmail.com>
2021-04-14 15:11:54 -0300

    tentando resolveu  23-26

--- desafio23.sql
+++ desafio23.sql
@@ -1,3 +1,5 @@
+SET SQL_SAFE_UPDATES = 0;
 update  northwind.order_details
 set discount = 30
 where unit_price < 100000;
+select * from order_details;
--- desafio26.sql
+++ desafio26.sql
@@ -1,3 +1,5 @@
+SET SQL_SAFE_UPDATES = 0;
 -- select * from   northwind.order_details
 delete from northwind.order_details
 where unit_price > 100000;
+select * from order_details;
