41d2baa27d4f7d82d3d002d935b1b5fb03889ada
UmberBn <humberto-bandeira@hotmail.com>
2021-04-14 16:40:34 -0400

    Resolvendo 'erro' no requisito 5, requisito 6 e 7 concluidos

--- desafio5.sql
+++ desafio5.sql
@@ -1,2 +1,1 @@
-SELECT * from northwind.products
-LIMIT 10 offset 3;
+SELECT * from northwind.products LIMIT 10 offset 3;
---
+++ desafio7.sql
@@ -0,0 +1,3 @@
+SELECT id FROM northwind.products
+ORDER BY id desc
+limit 5;
