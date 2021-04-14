c2044ce527b42eff1b3d0906580cd2031c36925d
adrianWarrior <adrianoforcellini>
2021-04-14 17:16:46 -0300

    requisito4

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(\*) FROM northwind.products;
+SELECT COUNT(*) FROM northwind.products;
 
 /* https://www.alura.com.br/artigos/select-count-count1-e-countnome-a-batalha-dos-counts-de-sql?gclid=Cj0KCQjwpdqDBhCSARIsAEUJ0hPaFM1QjJqrgeL4NHSAFpTzX4npoDlnuShisFL2YbG2l2k9w2r0rrsaAkWFEALw_wcB 
-porque o requisito não pediu apenas linhas não-nulas. */
+porque o requisito não pediu apenas linhas não-nulas, embora nenhuma seja. E * ao invés de \*, porque aparentemente \* é pra alguma outra versão, ou algum erro do navegador ou digitação.  */
