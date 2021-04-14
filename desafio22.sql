02dd6c12e1c4fdc5ab8596bd0f0794735509f351
victor718 <victorfelipeoliveira1@gmail.com>
2021-04-14 15:33:13 -0300

    fix

--- desafio22.sql
+++ desafio22.sql
@@ -1,4 +1,4 @@
 UPDATE northwind.order_details
 SET discount = 15
 WHERE id IN (SELECT * FROM(SELECT id FROM northwind.order_details
-	WHERE discount <> 15)menor);
+WHERE discount <> 15)menor);
--- desafio23.sql
+++ desafio23.sql
@@ -1,6 +1,6 @@
 UPDATE northwind.order_details
 SET discount = 30
 WHERE id IN (
-	SELECT * FROM(
-		SELECT id FROM northwind.order_details
-        WHERE unit_price < 10.0000)price);
+SELECT * FROM(
+SELECT id FROM northwind.order_details
+WHERE unit_price < 10.0000)price);
