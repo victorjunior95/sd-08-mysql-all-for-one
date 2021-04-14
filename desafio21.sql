6c47d442a34d4ce09559fdc2d805d2eab9e027fa
rodolfo_oliveira <rod.vgo@gmail.com>
2021-04-14 18:51:14 -0300

    desafio 21

---
+++ desafio21.sql
@@ -0,0 +1,3 @@
+insert into northwind.order_details 
+(order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+values (69, 80, 15.000, 15.000, 0, 2, null, null, 129), (69, 80, 15.000, 15.000, 0, 2, null, null, 129);
