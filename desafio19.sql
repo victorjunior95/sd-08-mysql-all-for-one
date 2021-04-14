72f94c35fe1a401f0ca71b0eb0db2b379bec5224
gabrieldeori <deori.pessoal@gmail.com>
2021-04-14 18:38:29 -0300

    :lady_beetle: Desafio 19, not naming correctly the column

--- desafio19.sql
+++ desafio19.sql
@@ -2,6 +2,6 @@
 employee_id igual a 5 ou 6, e que foram enviados através do método shipper_id
 igual a 2. Chame a coluna de orders_count. */
 
-SELECT COUNT(*) FROM northwind.orders AS orders_count
+SELECT COUNT(*) AS orders_count FROM northwind.orders
 WHERE employee_id IN (5,6) AND
 shipper_id = 2;
