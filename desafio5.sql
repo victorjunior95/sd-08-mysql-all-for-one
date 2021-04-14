43d540b6c2c13f6ba744953e0ff12af5d5987ef0
JRGCast <joao.ralph1@gmail.com>
2021-04-14 17:37:41 -0300

    alterado OFFSET de 4 para 3 no desafio5

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,3 @@
 -- 5 - Monte uma query que exiba os dados da tabela products a partir do quarto registro até o décimo terceiro, incluindo tanto um quanto o outro. Obs.: não use where ou order by.
 SELECT * FROM products
-LIMIT 13 
-OFFSET 4;
+LIMIT 13 OFFSET 3;
