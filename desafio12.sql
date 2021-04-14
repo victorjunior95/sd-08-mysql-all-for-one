27573c7f51f85117cdc5ad6ba52efac8e3148e85
Wesley Paulo <wesley_paulo00@hotmail.com>
2021-04-14 18:16:29 -0300

    Codeclimate fix

--- desafio12.sql
+++ desafio12.sql
@@ -1,3 +1,1 @@
-SELECT submitted_date FROM northwind.purchase_orders
-WHERE date(submitted_date)=date('2006-04-26');
-
+SELECT submitted_date FROM northwind.purchase_orders WHERE date(submitted_date)=date('2006-04-26');
