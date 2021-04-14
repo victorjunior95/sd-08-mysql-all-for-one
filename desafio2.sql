1dbe69a6599d11bc78574dcdc3200b5681072c8a
Digo-Castro <dygo.castro@gmail.com>
2021-04-14 15:23:57 -0300

    [Digo-Castro][Desafios 1 e 2] - OK

---
+++ desafio2.sql
@@ -0,0 +1,1 @@
+SELECT * FROm northwind.products;
--- tests/initialChallenges.spec.js
+++ tests/initialChallenges.spec.js
@@ -42,7 +42,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('3 - Escreva uma query que exiba os valores da coluna que representa a primary key da tabela `products`.', () => {
+  describe.skip('3 - Escreva uma query que exiba os valores da coluna que representa a primary key da tabela `products`.', () => {
     it('Verifica o desafio3', async () => {
       const challengeQuery = readFileSync('desafio3.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult3');
@@ -51,7 +51,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('4 - Conte quantos registros existem em `product_name` de `products`.', () => {
+  describe.skip('4 - Conte quantos registros existem em `product_name` de `products`.', () => {
     it('Verifica o desafio4', async () => {
       const challengeQuery = readFileSync('desafio4.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult4');
@@ -60,7 +60,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('5 - Monte uma query que exiba os dados da tabela `products` a partir do quarto registro até o décimo terceiro, incluindo tanto um quanto o outro. Obs.: não use `where` ou `order by`.', () => {
+  describe.skip('5 - Monte uma query que exiba os dados da tabela `products` a partir do quarto registro até o décimo terceiro, incluindo tanto um quanto o outro. Obs.: não use `where` ou `order by`.', () => {
     it('Verifica o desafio5', async () => {
       const challengeQuery = readFileSync('desafio5.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult5');
@@ -69,7 +69,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('6 - Exiba os dados das colunas `product_name` e `id` da tabela `products` de maneira que os resultados estejam em ordem alfabética dos nomes.', () => {
+  describe.skip('6 - Exiba os dados das colunas `product_name` e `id` da tabela `products` de maneira que os resultados estejam em ordem alfabética dos nomes.', () => {
     it('Verifica o desafio6', async () => {
       const challengeQuery = readFileSync('desafio6.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult6');
@@ -78,7 +78,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('7 - Mostre apenas os ids dos 5 últimos registros da tabela `products` (a ordernação deve ser baseada na coluna `id`).', () => {
+  describe.skip('7 - Mostre apenas os ids dos 5 últimos registros da tabela `products` (a ordernação deve ser baseada na coluna `id`).', () => {
     it('Verifica o desafio7', async () => {
       const challengeQuery = readFileSync('desafio7.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult7');
@@ -87,7 +87,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('8 - Faça uma consulta que retorne três colunas. Na primeira coluna, exiba a soma de `5 + 6` (essa soma deve ser realizada pelo SQL). Na segunda coluna deve haver a palavra \"de\". E por fim, na terceira coluna, exiba a soma de `2 + 8` (essa soma deve ser realizada pelo SQL). A primeira coluna deve se chamar \"A\", a segunda coluna deve se chamar \"Trybe\" e a terceira coluna deve se chamar \"eh\". Não use colunas pre-existentes, apenas o que for criado na hora.', () => {
+  describe.skip('8 - Faça uma consulta que retorne três colunas. Na primeira coluna, exiba a soma de `5 + 6` (essa soma deve ser realizada pelo SQL). Na segunda coluna deve haver a palavra \"de\". E por fim, na terceira coluna, exiba a soma de `2 + 8` (essa soma deve ser realizada pelo SQL). A primeira coluna deve se chamar \"A\", a segunda coluna deve se chamar \"Trybe\" e a terceira coluna deve se chamar \"eh\". Não use colunas pre-existentes, apenas o que for criado na hora.', () => {
     it('Verifica o desafio8', async () => {
       const challengeQuery = readFileSync('desafio8.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult8');
