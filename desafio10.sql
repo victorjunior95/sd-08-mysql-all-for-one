4b58bf0b1a80e33707f9c0fb119aa24ce8674e0e
rafasysop <rafaelcostamoura@gmail.com>
2021-04-14 18:46:42 -0300

    req 09 ao 19

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,1 @@
-SELECT * FROM northwind.purchase_orders where created_by >=3 order by created_by DESC , id ASC ;
+SELECT * FROM northwind.purchase_orders where created_by >= 3 order by created_by DESC , id ASC ;
