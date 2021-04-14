4f20289ba7a9a0a5f1b718841c3da129cb17208f
Doug-Fernando <doug.fernando95@gmail.com>
2021-04-14 17:15:03 -0300

    Corrige req 10 e req 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,1 +1,1 @@
-SELECT supplier_id FROM northwind.purchase_orders WHERE supplier_id = 1 OR supplier_id = 2;
+SELECT supplier_id FROM northwind.purchase_orders WHERE supplier_id = 1 OR supplier_id = 3;
