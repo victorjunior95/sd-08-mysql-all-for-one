646bda9d121523035b06948a61c6a267948ecfa4
adrianWarrior <adrianoforcellini>
2021-04-14 17:11:54 -0300

    requisito4

---
+++ desafio4.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(\*) FROM northwind.products;
+
+/* https://www.alura.com.br/artigos/select-count-count1-e-countnome-a-batalha-dos-counts-de-sql?gclid=Cj0KCQjwpdqDBhCSARIsAEUJ0hPaFM1QjJqrgeL4NHSAFpTzX4npoDlnuShisFL2YbG2l2k9w2r0rrsaAkWFEALw_wcB 
+porque o requisito não pediu apenas linhas não-nulas. */
