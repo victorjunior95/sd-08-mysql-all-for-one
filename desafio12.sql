e26aee113895081621d03e77cb9aeb65c43be6d8
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-04-14 16:19:34 -0300

    refazendo desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,1 +1,1 @@
-SELECT submitted_date FROM northwind.purchase_orders; WHERE submitted_date = CAST('2006-04-26' AS DATE);
+SELECT submitted_date FROM northwind.purchase_orders WHERE DATE(submitted_date) = '2006-04-26';
