e226f9ce679401bfbcd9470087e14c83dddb0aea
adrianWarrior <adrianoforcellini>
2021-04-14 18:08:37 -0300

    requisito 5 -  ajustando comentario

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,5 @@
 SELECT * FROM northwind.products LIMIT 10 OFFSET 3;
 
-/* consultei o repositório de Rita Jeveaux - https://github.com/tryber/sd-08-mysql-all-for-one/pull/44/commits/3a6ed79fdbabdaa122a7d33bdb9f9e929776f8a6
+/* consultei o repositório de Rita Jeveaux - 
+https://github.com/tryber/sd-08-mysql-all-for-one/pull/44/commits/3a6ed79fdbabdaa122a7d33bdb9f9e929776f8a6
 para compreender : https://dataschool.com/learn-sql/limit/ */
