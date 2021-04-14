495fb738aa518ec9715a739bf321fd8618be0e81
Luiz Reina <luizreeina@gmail.com>
2021-04-14 15:19:19 -0300

    correcao do desafio19

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(*) FROM northwind.orders WHERE (employee_id = 5 OR employee_id = 6) AND shipper_id = 2;
+SELECT COUNT(*) AS orders_count FROM northwind.orders WHERE (employee_id = 5 OR employee_id = 6) AND shipper_id = 2;
