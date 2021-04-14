d7c0227488042db2528a127180add0c3071d5451
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-04-14 19:16:09 -0300

    req22 e 23 fix

--- desafio22.sql
+++ desafio22.sql
@@ -1,4 +1,2 @@
-set SQL_SAFE_UPDATES = 0
-
 update northwind.order_details
 set discount = 15;
--- desafio23.sql
+++ desafio23.sql
@@ -1,5 +1,3 @@
-set SQL_SAFE_UPDATES = 0
-
 update northwind.order_details
 set discount = 30
 where unit_price < 10.000;
