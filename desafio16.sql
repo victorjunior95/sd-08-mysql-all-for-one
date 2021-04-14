4bd3d4f1b1ad4a1194f3350e37f203caf56936f7
Rosiele David <rosieleserpa@hotmail.com>
2021-04-14 20:23:42 -0300

    Desafio 16 concluído e atualizado

--- desafio16.sql
+++ desafio16.sql
@@ -1,1 +1,1 @@
-SELECT submitted_date FROM northwind.purchase_orders WHERE submitted_date BETWEEN '2006-01-26' AND '2006-03-31';
+SELECT submitted_date FROM northwind.purchase_orders WHERE submitted_date BETWEEN CAST('2006-01-26 00:00:00' AS DATETIME) AND CAST('2006-03-31 23:59:59' AS DATETIME );
