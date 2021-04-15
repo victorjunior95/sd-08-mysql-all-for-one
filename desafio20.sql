425e27adce65a745ebea7b00d450613c9cceee73
MarceloBorges <melombm@hotmail.com>
2021-04-15 12:12:15 -0500

    requisito 20

---
+++ desafio20.sql
@@ -0,0 +1,1 @@
+INSERT INTO order_details (order_id, product_id, quantity, unit_price,discount,status_id,date_allocated,purchase_order_id, inventory_id) VALUE(69,80,15.0000,15.0000,0,2,null,null,129);
