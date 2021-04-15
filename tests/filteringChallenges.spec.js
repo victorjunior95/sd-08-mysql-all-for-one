dbf7f903ad3dcb73521840ff544bee4dc686245a
Aline Debastiani <debastianialine@gmail.com>
2021-04-15 00:51:44 -0300

    cumpre desafio 11

--- tests/filteringChallenges.spec.js
+++ tests/filteringChallenges.spec.js
@@ -33,7 +33,7 @@ describe('Desafios sobre filtragem de dados', () => {
     });
   });
 
-  describe.only('10 - Mostre todos os dados da tabela `purchase_orders` em ordem decrescente ordenados por `created_by` em que o `created_by` é maior ou igual a 3. E como critério de desempate para a ordenação, ordene também os resultados pelo `id` de forma crescente.', () => {
+  describe('10 - Mostre todos os dados da tabela `purchase_orders` em ordem decrescente ordenados por `created_by` em que o `created_by` é maior ou igual a 3. E como critério de desempate para a ordenação, ordene também os resultados pelo `id` de forma crescente.', () => {
     it('Verifica o desafio10', async () => {
       const challengeQuery = readFileSync('desafio10.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult10');
@@ -44,7 +44,7 @@ describe('Desafios sobre filtragem de dados', () => {
     });
   });
 
-  describe('11 - Exiba os dados de `notes` da tabela `purchase_orders` em que seu valor de \"Purchase generated based on Order\" está entre 30 e 39, incluindo tanto o valor de 30 quanto de 39.', () => {
+  describe.only('11 - Exiba os dados de `notes` da tabela `purchase_orders` em que seu valor de \"Purchase generated based on Order\" está entre 30 e 39, incluindo tanto o valor de 30 quanto de 39.', () => {
     it('Verifica o desafio11', async () => {
       const challengeQuery = readFileSync('desafio11.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult11');
