fe9a52fd1998c3ea9080fa8f83d5f9df095b4587
jnpefo <jnpefo@gmail.com>
2021-04-14 22:59:08 -0300

    Req 24, 25, 26 e 27

--- desafio26.sql
+++ desafio26.sql
@@ -1,5 +1,2 @@
-INSERT IGNORE INTO northwind.order_details
-(order_id, product_id, quantity, unit_price, discount,
-status_id, date_allocated, purchase_order_id, inventory_id) VALUES
-('69', '80', '15.0000', '15.0000', '0','2', NULL, NULL, '129'),
-('69', '80', '15.0000', '15.0000', '0','2', NULL, NULL, '129');
+DELETE FROM northwinds.order_details
+WHERE unit_price > 10.000;
---
+++ desafio27.sql
@@ -0,0 +1,1 @@
+TRUNCATE northwinds.order_details;
