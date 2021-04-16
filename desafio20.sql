74c9a8cd5bfbe43b2bc489bf6cd0659a1fb8f059
adrianWarrior <adrianoforcellini>
2021-04-16 17:18:06 -0300

    requisito 20

---
+++ desafio20.sql
@@ -0,0 +1,7 @@
+INSERT INTO northwind.order_details
+(order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id) 
+VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
+
+/* consultei o repositório de Rita Jeveaux - 
+https://github.com/tryber/sd-08-mysql-all-for-one/pull/44/commits/3a6ed79fdbabdaa122a7d33bdb9f9e929776f8a6
+para compreender : https://www.w3schools.com/sql/sql_insert.asp */
