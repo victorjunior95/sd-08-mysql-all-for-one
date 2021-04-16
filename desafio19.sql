ec99c477bd5d0b6e4f80e01d464b921361424f7c
José Bernardo Johann <josebernardojohann@gmail.com>
2021-04-15 21:47:01 -0300

    Fixes queries

--- desafio19.sql
+++ desafio19.sql
@@ -1,2 +1,4 @@
-INSERT INTO northwind.orders(orders_count)
-VALUES (2);
+SELECT employee_id AS orders_count
+FROM northwind.orders
+WHERE employee_id = 5 OR employee_id = 6
+AND shipper_id = 2;
--- desafio9.sql
+++ desafio9.sql
@@ -1,3 +1,3 @@
-SELECT *
+SELECT notes
 FROM northwind.purchase_orders
 WHERE notes IS NOT NULL;
