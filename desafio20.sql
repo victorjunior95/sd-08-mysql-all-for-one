2d1d14c1d3cb074fcb01d7973ef0e757c6424cf0
Iury Nathan <iurynathan@gmail.com>
2021-04-19 09:01:10 -0300

    esqueci de salvar a correção, jesus kkkkk

--- desafio20.sql
+++ desafio20.sql
@@ -7,5 +7,5 @@ INSERT INTO northwind.order_details (
     status_id, 
     date_allocated, 
     purchase_order_id, 
-    inventory_id,
+    inventory_id
 ) VALUES ( 69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129 );
